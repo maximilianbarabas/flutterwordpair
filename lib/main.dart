39b3e23bbfcaaa0320de283a3798ee875e68aaac
Maxim Barabas <maximbarabas@Maxim-MacBook-Air.local>
2023-05-04 00:34:42 +0200

    second commit_added random name widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,25 @@ class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
 
   Widget _buildList() {
-    return ListView(
-        padding: const EdgeInsets.all(16), itemBuilder: (contect, item) {});
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)));
   }
 
   Widget build(BuildContext context) {
